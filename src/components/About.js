66e298ae8efae236cc9ea54830d5db2f5aa80764
AttilaMarusij <92247516+AttilaMarusij@users.noreply.github.com>
2021-11-11 08:23:09 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -33,7 +33,7 @@ export default function About() {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./coding.svg"
+            src="./my-photo-removebg-preview.png"
           />
         </div>
       </div>
